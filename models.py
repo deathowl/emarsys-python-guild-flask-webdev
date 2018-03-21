18a0cf08933566d9e3294a7aab3d39a96f9df71b
Balint Csergo <balint.csergo@emarsys.com>
2018-03-21 10:34:06 +0100

    migrations via alembic

--- models.py
+++ models.py
@@ -1,9 +1,14 @@
-from sqlalchemy import ForeignKey, Column, String, Integer, Boolean
+from datetime import datetime
+
+from sqlalchemy import ForeignKey, Column, String, Integer, Boolean, DateTime
 from sqlalchemy import true as db_true
 from sqlalchemy import false as db_false
 from sqlalchemy.event import listens_for
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql import func
+
+
 
 from db import db
 
@@ -12,7 +17,7 @@ Base = declarative_base()
 
 class Users(Base):
     __tablename__ = "users"
-    id = Column(db.Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
     name = Column(String(30), unique=True, nullable=False)
     password = Column(String(80), nullable=False, default="")
     description = Column(String(255), nullable=True)
@@ -25,19 +30,20 @@ class Users(Base):
 
 class Videos(Base):
     __tablename__ = "videos"
-    id = Column(db.Integer(), primary_key=True)
+    id = Column(Integer(), primary_key=True)
     title = Column(String(255))
     description = Column(String(4032))
-    userid = Column(String(255), ForeignKey('users.id'))
+    userid = Column(Integer(), ForeignKey('users.id'))
     user = relationship("Users", foreign_keys=[userid])
     path = Column(String(400), nullable=False)
+    uploaded_at = Column(DateTime, nullable=False, default=datetime.utcnow, server_default=func.now())
 
 
 class Comments(Base):
     __tablename__ = "comments"
     id = Column(db.Integer(), primary_key=True)
-    userid = Column(String(255), ForeignKey('users.id'))
-    videoid = Column(String(255), ForeignKey('videos.id'))
+    userid = Column(Integer(), ForeignKey('users.id'))
+    videoid = Column(Integer(), ForeignKey('videos.id'))
     user = relationship("Users", foreign_keys=[userid])
     video = relationship("Videos", foreign_keys=[videoid])
     text = Column(String(4096))
